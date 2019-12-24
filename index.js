d92cf438c4038233ab7ed6b2f6f4c7753427a766
Samuel Catley <scp94373@gmail.com>
2019-12-24 12:21:39 +0000

    update - ex 3

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var sliderPan;
 
 var button;
 
+var jumpButton;
+
 
 //setup canvas and load song
 function setup() {
@@ -18,11 +20,34 @@ function setup() {
     song.setVolume(0.1)
     button = createButton("Play");
     button.mousePressed(togglePlaying);
+
+    jumpButton = createButton("jump")
+    jumpButton.mousePressed(jumpSong);
+
     //configuring variables for creating sliders (createSlider part of P5)
     sliderRate = createSlider(0, 3, 1, 0.01);
     sliderPan = createSlider(-1, 1, 0, 0.01);
+
+    // song.addCue(5, changeBackground);
+    song.addCue(2, changeBackground, color(0,0,255));
+    song.addCue(4, changeBackground, color(0,255,255));
+    song.addCue(6, changeBackground, color(255,255,255));
+    
     
 }
+
+function changeBackground(col) {
+    background(col)
+    // background(random(255),random(255),random(255))
+}
+function jumpSong() {
+    var len = song.duration();
+    var time = random(len);
+    song.jump(time)
+    //other extensions
+    //song.currentTime();
+}
+
 
 function togglePlaying() {
     if(!song.isPlaying()) {
@@ -51,7 +76,11 @@ function loaded() {
 
 //draw function, canvas and slider settings
 function draw() {
-    background(0);
+    //if song's time is greater than 5, change background colour
+    // if(song.currentTime() > 5) {
+    //     background(song.currentTime()*10, 0, 255)
+    // }
     // song.pan(sliderPan.value());
     song.rate(sliderRate.value());
 }
+
