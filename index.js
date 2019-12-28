b3acab41db71bae15634ebe94b2ea639dc276a1e
Samuel Catley <scp94373@gmail.com>
2019-12-28 15:12:01 +0000

    More Tracks, default shape
    default shape for visualiser, added more tracks.

--- index.js
+++ index.js
@@ -1,86 +1,112 @@
-//Variable for song object
 var song;
-// var sliderVolume;
-//Variable for rate of playing speed
-var sliderRate;
-//Variable for panning sound from left speaker to right
-var sliderPan;
-
+var fft;
 var button;
 
-var jumpButton;
+var numOfBands;
 
 
-//setup canvas and load song
+function preload() {
+    song = loadSound('audio/bensound-summer.mp3')
+    
+}
+
 function setup() {
-    createCanvas(200, 200);
-    //load song and once loaded, run "loaded" function, loadSound oart of P5
-    song = loadSound("audio/rainbow.mp3", loaded);
-    //set volume
-    song.setVolume(0.1)
-    button = createButton("Play");
-    button.mousePressed(togglePlaying);
-
-    jumpButton = createButton("jump")
-    jumpButton.mousePressed(jumpSong);
-
-    //configuring variables for creating sliders (createSlider part of P5)
-    sliderRate = createSlider(0, 3, 1, 0.01);
-    sliderPan = createSlider(-1, 1, 0, 0.01);
-
-    // song.addCue(5, changeBackground);
-    song.addCue(2, changeBackground, color(0,0,255));
-    song.addCue(4, changeBackground, color(0,255,255));
-    song.addCue(6, changeBackground, color(255,255,255));
+    //create canvas
+    createCanvas(256, 256)
+    colorMode(HSB)
+    //anglemode is degrees as opposed to radians as default in canvas
+    angleMode(DEGREES);
+    //create button to play/pause
+    button = createButton('Play/Pause')
+    //event handler, when button is pressed, fun playPause function
+    button.mousePressed(playPause)
+    //set volume of music to 0.1
+    song.setVolume(0.1);
+    //fft object, 0 smoothing, 256 freq bands 
+    numOfBands = 128;
+    fft = new p5.FFT(0.6, numOfBands);
+    // w = width / numOfBands;
     
-    
-}
 
-function changeBackground(col) {
-    background(col)
-    // background(random(255),random(255),random(255))
 }
-function jumpSong() {
-    var len = song.duration();
-    var time = random(len);
-    song.jump(time)
-    //other extensions
-    //song.currentTime();
-}
-
-
-function togglePlaying() {
-    if(!song.isPlaying()) {
+//function to play/pause
+function playPause() {
+    if (!song.isPlaying()) {
         song.play();
-        song.setVolume(0.3);
-        //change name of button to pause
-        button.html("Pause")
-        //change name of button to stop
-        // button.html("Stop")
     } else {
-        //song pauses
         song.pause();
-        //song stops
-        //song.stop();
-        button.html("play")
     }
-    
-    
 }
 
-// function of playing song
-function loaded() {
-    // song.play();
-    console.log('loaded')
-}
 
-//draw function, canvas and slider settings
 function draw() {
-    //if song's time is greater than 5, change background colour
-    // if(song.currentTime() > 5) {
-    //     background(song.currentTime()*10, 0, 255)
-    // }
-    // song.pan(sliderPan.value());
-    song.rate(sliderRate.value());
-}
+    //background black
+    background(0);
+    //spectrum variable to contain the fft analyze results
+    var spectrum = fft.analyze();
+    //stroke white
+    // stroke(255)
+    noStroke();
+    translate(width/2, height/2);
+    
 
+    // beginShape();
+    //for every... less than spectrum length
+    fft.getEnergy('bass')
+    for(var i = 0; i < spectrum.length; i++) {
+        //amp/vol variable for each individual band/freq
+        // console.log(fft.getEnergy('bass'))
+        var amp = spectrum[i];
+        
+        //Angle - changed from 200 to 100 for interesting effect
+        var angle = map(i, 0, spectrum.length, 0, 360);
+        // var angle = map(i, 0, 100, 0, 360);
+        // console.log(amp)
+        // var r = map(amp, 0, spectrum.length, 1, 200);
+        var r = map(amp, 0, spectrum.length, 10, 300);
+        var x = r * cos(angle) /2;
+        var y = r * sin(angle) /2;
+        
+        // var x = r * sin(angle);
+        // var y = r * cos(angle);
+    
+        //Star----------------------
+        //comment out fill for dancing white star
+        //colour in star depending on levels
+        rotate(i)
+            if(fft.getEnergy('mid') > 140 && fft.getEnergy('bass') > 180) {
+                fill(i, 255, 255, 0.25)
+            } else {
+                // fill(i, 255, 255, 0.1)
+                fill(i, 255, 255, 0.1)
+            }
+            // fill(255, 255, 255, 0.3)
+            // rotate(50)
+
+            ellipse(0, 0, x, y)
+            ellipse(0, 0, y, x)
+
+        //Vertical Eye------------------
+        // fill(255, 255, 255, i*0.9)
+        // var reverseNumbers = map(i, 0, 255, 0, 150)
+        // fill(reverseNumbers, 255, 255, 0.3)
+        // ellipse(0, 0, y, x)
+        
+        // ellipse(0, 0, x, y)
+        
+
+        //Lines-----------------------
+        // stroke(i, 255, 255)
+        // line(0, 0, x, y)
+
+
+        //y to equal height of volume/amp of band/freq, from 0 - 256, to height of canvas and 0
+        // var y = map(amp, 0, 256, height-10, 0);
+        // 
+        // //line from [i], height of canvas (to start at bottom), draw to [i], variable Y
+        // rect(i * w, y, w, height - y);
+    }
+    // endShape();
+   
+    
+}
